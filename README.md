49cc607f4aca7bcefb168be8b7881a50030976e3
JackPu <kakashjack@gmail.com>
2016-08-24 18:09:55 +0800

    add files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-native-percentage-circle
-React Native Percentage Circle
+
+<img src="https://camo.githubusercontent.com/3968e0ea333f986243e5e631b01d1ee2f45552bd/68747470733a2f2f62616467652e667572792e696f2f6a732f72656163742d6e61746976652d7363726f6c6c61626c652d7461622d766965772e737667"/>
+
+
+React-Native-Percentage-Cirlce is a component which supports you define your percent and draw the circle.And also you can use it as progress bar.
+
+
---
+++ index.js
@@ -0,0 +1,152 @@
+/** React Native Percentage Circle
+** @github  https://github.com/JackPu/react-native-percentage-circle 
+** React Native Version >=0.25
+**/
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  circle: {
+    overflow: 'hidden',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e3e3e3',
+  },
+  leftWrap: {
+    overflow:'hidden',  
+    position: 'absolute',
+    top:0, 
+  },
+  rightWrap: {
+    position: 'absolute',
+
+  },
+    
+  loader:{
+    position:'absolute',
+    left:0,
+    top:0,
+    borderRadius:1000,
+    
+  },
+  
+  innerCircle: {
+    overflow: 'hidden',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 11,
+    color: '#888',
+
+  },
+});
+
+
+  
+class PercentageCircle extends Component {
+   
+  propTypes:{
+    color: React.PropTypes.string,
+    radius: React.PropTypes.number,
+    percent: React.PropTypes.number,
+    disabled: React.PropTypes.bool  
+  }
+  
+  constructor(props) {
+    super(props);
+    
+    let percent = this.props.percent;
+    let leftTransformerDegree = '0deg';
+    let rightTransformerDegree = '0deg';
+    if(percent >= 50) {
+      rightTransformerDegree = '180deg';
+      leftTransformerDegree = (percent-50)*3.6 + 'deg';
+    }else {
+      rightTransformerDegree = percent * 3.6 + 'deg'; 
+    }
+    
+    this.state = {
+      percent:this.props.percent,
+      leftTransformerDegree: leftTransformerDegree,
+      rightTransformerDegree: rightTransformerDegree
+    };
+    
+    
+  }
+
+  render() {
+    if(this.props.disabled) {
+      return(
+        <View style={[styles.circle,{
+          width:this.props.radius*2,
+          height: this.props.radius*2,
+          borderRadius:this.props.radius
+        }]}>
+          <Text style={styles.text}>{this.props.disabledText}</Text>
+        </View>  
+      );
+    }
+    return (
+      <View style={[styles.circle,{
+        width:this.props.radius*2,
+        height: this.props.radius*2,
+        borderRadius:this.props.radius
+      }]}>
+        <View style={[styles.leftWrap,{
+          width: this.props.radius,
+          height: this.props.radius * 2,
+          left:0,
+        }]}>
+          <View style={[styles.loader,{
+            left: this.props.radius,
+            width:this.props.radius,
+            height: this.props.radius*2,
+            borderTopLeftRadius:0,
+            borderBottomLeftRadius:0,
+            backgroundColor:this.props.color,
+            transform:[{translateX:-this.props.radius/2},{rotate:this.state.leftTransformerDegree},{translateX:this.props.radius/2}],  
+          }]}></View>
+        </View>
+        <View style={[styles.leftWrap,{
+          left:this.props.radius,
+          width: this.props.radius,
+          height: this.props.radius * 2,
+          left:this.props.radius,
+        }]}>
+          <View style={[styles.loader,{
+            left:-this.props.radius,
+            width:this.props.radius,
+            height: this.props.radius*2,
+            borderTopRightRadius:0,
+            borderBottomRightRadius:0,
+            backgroundColor:this.props.color,
+            transform:[{translateX:this.props.radius/2},{rotate:this.state.rightTransformerDegree},{translateX:-this.props.radius/2}],  
+          }]}></View>
+        </View>  
+            <View style={[styles.innerCircle,{
+              width:(this.props.radius-2)*2 , 
+              height:(this.props.radius-2)*2,
+              borderRadius:this.props.radius - 2,
+              backgroundColor: '#fff',
+            }]}>
+              <Text style={styles.text}>{this.state.percent}%</Text>
+            </View>
+            
+        </View>
+    );    
+
+  }
+
+};
+
+module.exports = PercentageCircle;
