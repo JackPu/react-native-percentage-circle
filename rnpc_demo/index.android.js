8318b804a68ed4cda3626aa21bd21964a4b4da77
JackPu <kakashjack@gmail.com>
2017-04-20 14:40:36 +0800

    update package

--- rnpc_demo/index.android.js
+++ rnpc_demo/index.android.js
@@ -9,23 +9,85 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+import PercentageCircle from 'react-native-percentage-circle';
+import TimerMixin from 'react-timer-mixin';
+
 
 class rnpc_demo extends Component {
+    
+  constructor(props) {
+    super(props);
+    this.state = {
+      percent: 10,
+    };
+    
+  }
+  
+  componentDidMount() {
+    let self = this;
+    this.intval = setInterval(
+      () => { 
+        let s = this.state.percent % 100;
+        s += 1;
+        self.setState({
+          percent: s,
+        })
+      },
+      1000/60
+    );  
+  }
+  
+  componentWillUnmount() {
+    //this.intval && clearInterval(this.intval);
+  }
+    
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Use React Native Percentage Circle
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={styles.row}>
+          <View style={styles.item}>
+            <PercentageCircle radius={35} percent={50} color={"#3498db"}></PercentageCircle>  
+            <Text style={[styles.percentText]}> 50% </Text>
+          </View>
+          <View style={styles.item}>
+            <PercentageCircle radius={35} percent={30} color={"#f39c12"}>
+              <Text style={styles.checkin}>30</Text>
+              <Text style={styles.desc}>人已签到</Text>
+            </PercentageCircle>  
+            <Text style={[styles.percentText]}> 30% </Text>
+          </View>
+          <View style={styles.item}>
+            <PercentageCircle radius={35} bgcolor="#fff" percent={75} color={"#2ecc71"}></PercentageCircle>  
+            <Text style={[styles.percentText]}> 75% </Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.item}>
+            <PercentageCircle radius={60} bgcolor="#fff" borderWidth={4} percent={90} color={"#34495e"}>
+              <View style={{marginLeft:30,alignItems:'center',flexDirection:'row'}}>
+                <Image style={{width:25,height:25}} source={require('./images/shoes.png')} />
+                <Text style={{flex:1,fontSize:13}}>步数</Text>
+              </View>
+              <View>
+                <Text style={{fontSize:23,color:'#34495e'}}>20000</Text>
+              </View>
+            </PercentageCircle>  
+            <Text style={[styles.percentText]}> 90% </Text> 
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.item}>
+            <PercentageCircle radius={60} percent={this.state.percent} color={"#9b59b6"}></PercentageCircle>  
+            <Text style={[styles.percentText]}> {this.state.percent} %</Text> 
+          </View>
+        </View>
+        
       </View>
     );
   }
@@ -34,20 +96,47 @@ class rnpc_demo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop:66,
+    backgroundColor: '#fff',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 16,
     textAlign: 'center',
-    margin: 10,
+    margin: 20,
+  },
+  row:{
+    //height:0,
+    flexDirection:'row',
+    alignItems:'center',
+    flexWrap:'wrap',
+    marginBottom:40,
+  },
+  item:{
+    flex:.33,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  percentText:{
+    fontSize:15,
+    paddingTop:10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  
+  checkin: {
+    fontSize:20,
+    color: '#f39c12',
+  },
+  desc: {
+    fontSize:12,
+    color: '#999',
+  },
+ 
 });
 
 AppRegistry.registerComponent('rnpc_demo', () => rnpc_demo);
